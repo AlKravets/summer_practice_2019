671cde9ab2fea10420bece3d55bd817ccf504b88
Aleksey Kravets <alekravets976@gmail.com>
2019-08-07 19:28:46 +0300

    first version median blur

--- median_blur.py
+++ median_blur.py
@@ -39,17 +39,37 @@ def photo_extension(img: np.ndarray, k: int) -> np.ndarray:
     return new_img
 
 
-'''
-def medianblur_gray (img: np.ndarray, k: int) -> np.ndarray:
-    
+# поиск медианы на полученном окне со стороной k
+def find_median (window : np.ndarray):
+    if len(window.shape) ==2:
+        return int(np.median(window))
+    else:
+        res = []
+        #print(window)
+        for i in range(window.shape[2]):
+            res.append(int(np.median(window[::,::,i])))
+        return res
 
-    
-    return 
 
-'''
+def median_blur(img: np.ndarray, k: int)->  np.ndarray:
+    new_img = photo_extension(img,k)
+    res = img
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+        
+            n_i = i+ k//2
+            n_j = j+k//2
+            res[i][j] = find_median(new_img[n_i-k//2:n_i+k//2+1, n_j-k//2:n_j+k//2+1])             
+        print(i)
+    return res
 
 if __name__ == '__main__':
     img = cv2.imread('test_rgb.jpg')
-    res = photo_extension(img, 51)
-
+    res = median_blur(img, 21)
     cv2.imwrite('results//res1.jpg', res)
+
+    new_res = cv2.medianBlur(img, 21)
+    cv2.imwrite('results//res_opencv.jpg', new_res)
+
+    test = res - new_res
+    cv2.imwrite('results//test.jpg', test)
